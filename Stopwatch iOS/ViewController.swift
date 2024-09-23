9785ba63ca4792e03474dc654f03bb840a52a7dc
nuaman <nuaman.s@axeltechnologies.com>
2024-09-23 12:36:34 +0530

    Initial Commit

---
+++ Stopwatch iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stopwatch iOS
+//
+//  Created by Muhammed Nuaman Sadiq on 23/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Stopwatch iOSUITests/Stopwatch_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Stopwatch_iOSUITestsLaunchTests.swift
+//  Stopwatch iOSUITests
+//
+//  Created by Muhammed Nuaman Sadiq on 23/09/24.
+//
+
+import XCTest
+
+final class Stopwatch_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
